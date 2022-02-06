76519a1ef638c4fd773d640f94b3aad847f4a77f
Winston Jones <winston.jones@sikt.no>
2022-02-06 20:33:36 +0100

    new javascript with buttons and more- unfinnished

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
+document.querySelectorAll("input").forEach(
+    inputfield => {
+        inputfield.addEventListener("blur", function (event) {
+            if (inputfield.value === "") {
+                inputfield.classList.remove("hascontent")
+            } else {
+                inputfield.classList.add("hascontent")
+            }
+        })
+    });
 
+document.querySelectorAll("textarea").forEach(
+    field => {
+        field.addEventListener("blur", function (event) {
+            if (field.value === "") {
+                field.classList.remove("hascontent")
+            } else {
+                field.classList.add("hascontent")
+            }
+        })
+    });
+
+function openSideNav() {
+    document.getElementsByClassName("nav-drawer")[0].style.display = "block";
+}
+function closeSideNav() {
+    document.getElementsByClassName("nav-drawer")[0].style.display = "none";
+}
