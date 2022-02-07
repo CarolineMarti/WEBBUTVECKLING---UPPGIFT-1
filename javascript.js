8dc6545ca87a930c62acb209869f59827e909852
CarolineMarti <carolinemartinussen@chasacademy.se>
2022-02-07 17:18:09 +0100

    updated comments and other minor fixes

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+/*This code retrieves all INPUT-fields on the page and adds eventlistener 
+ to the field when ”blur” event occurs, and depending 
+on whether there is a value in the INPUT-field or not, it adds or removes the 
+css class "hascontent ”*/
 document.querySelectorAll("input").forEach(
     inputfield => {
         inputfield.addEventListener("blur", function (event) {
@@ -8,7 +12,10 @@ document.querySelectorAll("input").forEach(
             }
         })
     });
-
+/*This code retrieves all TEXTAREA-fields on the page and adds eventlistener 
+ to the field when ”blur” event occurs, and depending 
+on whether there is a value in the TEXTAREA-field or not, it adds or removes the 
+css class "hascontent ”*/
 document.querySelectorAll("textarea").forEach(
     field => {
         field.addEventListener("blur", function (event) {
@@ -20,9 +27,13 @@ document.querySelectorAll("textarea").forEach(
         })
     });
 
+/*This code uses javascript to add a click event listener on the 
+menu items to OPEN the Collapse navbar*/
 function openSideNav() {
     document.getElementsByClassName("nav-drawer")[0].style.display = "block";
 }
+/*This code uses javascript to add a click event listener on the 
+ menu items to CLOSE the Collapse navbar*/
 function closeSideNav() {
     document.getElementsByClassName("nav-drawer")[0].style.display = "none";
 }
